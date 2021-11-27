8f73be71ec24325cd14b12c8c7d54a4197b020e2
VishrutMargam <vishrutm@axioned.com>
2021-11-27 16:34:43 +0530

    added video functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,17 @@ $(document).ready(() => {
         $('nav').toggleClass('showNav')
         $('html, body').css("overflow", "visible")
     })
+
+    // play video functionality
+    $('article:first-child, .articles-list .wrapper ul li:nth-of-type(2) figure, .articles-list .wrapper ul li:nth-of-type(3) figure').click(() => {
+        $('.video .wrapper').fadeIn();
+        $('.video .wrapper').css('display', 'flex');
+        $('video').trigger('play');
+    })
+
+    // close video functionality
+    $('#close-video').click(() => {
+        $('video').trigger('pause');
+        $('.video .wrapper').fadeOut();
+    })
 })
